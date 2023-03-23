110d9439ca789b6a20598113888a0d8e0cfcfd41
Md Liton Mahmud <litonmahmud0023@gmail.com>
2023-03-24 03:04:07 +0600

    complate-product-container

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './Product.css';
 const Product = (props) => {
     console.log(props.product)
-   const {name,img,age,time}=props.product;
+   const {name,img,age,time,detail}=props.product;
     return (
         <div className='product-container'>
             <div className='product-info'>
                 <img src={img} alt="" />
                 <h1>{name}</h1>
-                <p>Age:{age}</p>
-                <p>Time:{time}</p>
+                <p className='details'>{detail}</p>
+                <p>For-Age:{age}</p>
+                <p>Time required:{time}</p>
             </div>
             <button className='product-btn'>Add To List</button>
         </div>
